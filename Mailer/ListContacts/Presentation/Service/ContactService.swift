0f4a5347629e4fab9a224691cd07cd06f9ed96ed
Anderson Costa <anderson.costa@mwangops.co.uk>
2023-11-05 00:01:28 +0000

    chore: read contacts from repository

--- Mailer/ListContacts/Presentation/Service/ContactService.swift
+++ Mailer/ListContacts/Presentation/Service/ContactService.swift
@@ -10,39 +10,14 @@ import Foundation
 
 final class ContactService {
 
-    private let csvReader: CSVReading
-    private let filePath: String
     private let repository: ContactRepository
 
-    init(csvReader: CSVReading, filePath: String, repository: ContactRepository) {
-        self.csvReader = csvReader
-        self.filePath = filePath
+    init(repository: ContactRepository) {
         self.repository = repository
     }
 
     func loadContacts(completion: @escaping ([Contact]) -> Void) {
-        csvReader.open(path: filePath)
-        var contacts: [Contact] = []
-        while let row = csvReader.readNextRow() {
-            let contact = Contact(
-                name: String(row[0]),
-                contactMethod: contactMethod(fromAddress: String(row[1]))
-            )
-            contacts.append(contact)
-        }
+        let contacts = repository.contacts
         completion(contacts)
-        csvReader.close()
-    }
-
-    private func contactMethod(fromAddress address: String) -> String {
-        let contactMethodToCountryMapping: [String: [String]] = [
-            "e-Mail": ["Czech Republic", "Saint Lucia"],
-            "Post": ["Italy", "Australia", "Finland"]
-        ]
-        let contactMethod = contactMethodToCountryMapping.first {
-            let country = address.components(separatedBy: "|").last ?? ""
-            return $0.value.contains(country)
-        }
-        return contactMethod?.key ?? "SMS"
     }
 }
--- MailerTests/ListContacts/Presentation/Service/ContactServiceTests.swift
+++ MailerTests/ListContacts/Presentation/Service/ContactServiceTests.swift
@@ -12,34 +12,8 @@ import XCTest
 
 final class ContactServiceTests: XCTestCase {
 
-    func test_loadContacts_interacts_with_reader_in_correct_order() {
-        let reader = FakeCSVReader()
-        let filePath = "somePathLocation"
-        let sut = ContactService(csvReader: reader, filePath: filePath, repository: FakeRepository())
-        sut.loadContacts { _ in }
-        let expectedInteractions: [FakeCSVReader.Interaction] = [
-            .open(filePath), .readRow, .readRow, .close
-        ]
-        XCTAssertEqual(reader.interactions, expectedInteractions)
-    }
-
-    func test_loadContacts_returns_empty_list_when_file_is_empty() {
-        let reader = FakeCSVReader()
-        reader.readNextRowReturn = []
-        let sut = ContactService(csvReader: reader, filePath: "DUMMY", repository: FakeRepository())
-        var expectedContacts: [Contact]?
-        sut.loadContacts() { expectedContacts = $0 }
-        XCTAssertEqual(expectedContacts, [])
-    }
-
     func test_loadContacts_returns_correct_contacts() {
-        let reader = FakeCSVReader()
-        reader.readNextRowReturn = [
-            ["John Appleseed", "Hertfordshire|Finland"],
-            ["Velma Combs", "306 Rhoncus. St.|Czech Republic"],
-            ["Porter Coffey", "Palo Alto|Cameroon"]
-        ]
-        let sut = ContactService(csvReader: reader, filePath: "DUMMY", repository: FakeRepository())
+        let sut = ContactService(repository: FakeRepository())
         var expectedContacts: [Contact]?
         sut.loadContacts() { expectedContacts = $0 }
         XCTAssertEqual(expectedContacts, Contact.dummyData)
@@ -47,35 +21,7 @@ final class ContactServiceTests: XCTestCase {
 
     // MARK: Helpers
 
-    private final class FakeCSVReader: NSObject, CSVReading {
-
-        enum Interaction: Equatable {
-            case open(String)
-            case readRow, close
-        }
-
-        private(set) var interactions: [Interaction] = []
-        var readNextRowReturn: [[Substring]] = [["Name", "Address"]]
-
-        func readNextRow() -> [Substring]? {
-            interactions.append(.readRow)
-            if readNextRowReturn.isEmpty {
-                return nil
-            } else {
-                return readNextRowReturn.removeFirst()
-            }
-        }
-        
-        func open(path: String) {
-            interactions.append(.open(path))
-        }
-        
-        func close() {
-            interactions.append(.close)
-        }
-    }
-
-    final class FakeRepository: ContactRepository {
-        let contacts: [Contact] = []
+    private final class FakeRepository: ContactRepository {
+        let contacts: [Contact] = Contact.dummyData
     }
 }
