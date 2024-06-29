6b48360d4b13a0d2ac149246d0a5f2d2e5a70747
Anderson Costa <anderson.costa@mwangops.co.uk>
2024-06-29 16:32:50 +0100

    test: ensure test suit works

---
+++ MailerTests/ContactsTableViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ContactsTableViewControllerTests.swift
+//  MailerTests
+//
+//  Created by Anderson Costa on 29/06/2024.
+//  Copyright © 2024 Trainline. All rights reserved.
+//
+
+import XCTest
+
+final class ContactsTableViewControllerTests: XCTestCase {
+
+    func test_zero() {
+        XCTAssertTrue(true)
+    }
+}
