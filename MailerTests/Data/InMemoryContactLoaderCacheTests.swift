a602391fbb352cd1ae63d1ef5e7e73aa1f6dbebc
Anderson Costa <anderson.costa@mwangops.co.uk>
2024-06-30 16:29:02 +0100

    feat: return received contacts

--- MailerTests/Data/InMemoryContactLoaderCacheTests.swift
+++ MailerTests/Data/InMemoryContactLoaderCacheTests.swift
@@ -8,18 +8,20 @@
 
 import XCTest
 
+@testable import Mailer
+
 final class InMemoryContactLoaderCache {
 
-    private let loadContacts: () -> Void
+    private let loadContacts: (([ContactViewModel]) -> Void) -> Void
     private var loadCalled = false
 
-    init(loadContacts: @escaping () -> Void) {
+    init(loadContacts: @escaping (([ContactViewModel]) -> Void) -> Void) {
         self.loadContacts = loadContacts
     }
 
-    func load() {
+    func load(completion: @escaping ([ContactViewModel]) -> Void) {
         guard !loadCalled else { return }
-        loadContacts()
+        loadContacts { contacts in completion(contacts) }
         loadCalled = true
     }
 }
@@ -28,19 +30,30 @@ final class InMemoryContactLoaderCacheTests: XCTestCase {
 
     func test_load_contacts_on_first_access() {
         var loaderCalled = false
-        let sut = InMemoryContactLoaderCache { loaderCalled = true }
-        sut.load()
+        let sut = InMemoryContactLoaderCache { _ in loaderCalled = true }
+        sut.load { _ in }
         XCTAssertTrue(loaderCalled)
     }
 
-    func test_read_from_cache_on_subsequent_access() {
+    func test_does_not_load_contacts_on_subsequent_access() {
         var loadCount = 0
-        let sut = InMemoryContactLoaderCache { loadCount += 1 }
+        let sut = InMemoryContactLoaderCache { _ in loadCount += 1 }
 
-        sut.load()
-        sut.load()
-        sut.load()
+        sut.load { _ in }
+        sut.load { _ in }
+        sut.load { _ in }
 
         XCTAssertEqual(loadCount, 1)
+    }
+
+    func test_returns_received_contacts_on_completion() {
+        let testContacts = [
+            ContactViewModel(name: "Jane Doe", contactMethod: .email),
+            ContactViewModel(name: "Clark Kent", contactMethod: .post)
+        ]
+        var receivedContacs: [ContactViewModel] = []
+        let sut = InMemoryContactLoaderCache { completion in completion(testContacts) }
+        sut.load { contacts in receivedContacs = contacts }
+        XCTAssertEqual(receivedContacs, testContacts)
     }
 }
