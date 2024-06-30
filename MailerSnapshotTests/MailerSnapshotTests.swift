c5b5a90f8ce6ef51070d7838ab5fb0fb878d7800
Anderson Costa <anderson.costa@mwangops.co.uk>
2024-06-30 13:40:36 +0100

    test: ensure contacts display correctly

--- MailerSnapshotTests/MailerSnapshotTests.swift
+++ MailerSnapshotTests/MailerSnapshotTests.swift
@@ -6,31 +6,33 @@
 //  Copyright © 2024 Trainline. All rights reserved.
 //
 
+import SnapshotTesting
 import XCTest
+
+@testable import Mailer
+
+// Snapshots recorded with Xcode 15.4 on iPhone 15 Pro, iOS 17.5
 
 final class MailerSnapshotTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func test_contact_list_screen() {
+        let dummyContacts = [
+            ContactViewModel(name: "Shelby Macias", contactMethod: "Post"),
+            ContactViewModel(name: "Porter Coffey", contactMethod: "Post"),
+            ContactViewModel(name: "Noelani Ward", contactMethod: "eMail"),
+            ContactViewModel(name: "Lillian Cotton", contactMethod: "SMS"),
+            ContactViewModel(name: "Ursa Faulkner", contactMethod: "eMail"),
+            ContactViewModel(name: "Cailin Mckinney", contactMethod: "Post"),
+            ContactViewModel(name: "Heidi Lawson", contactMethod: "SMS"),
+            ContactViewModel(name: "Mariam Madden", contactMethod: "SMS"),
+            ContactViewModel(name: "Velma Combs", contactMethod: "eMail")
+        ]
+        let sut = ContactsTableViewController(
+            displayDetails: { _ in }, loadContacts: { completion in completion(dummyContacts) }
+        )
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let navController = UINavigationController(rootViewController: sut)
+        navController.navigationBar.prefersLargeTitles = true
+        assertSnapshot(of: navController, as: .image)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
