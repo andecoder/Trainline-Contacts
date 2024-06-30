dcc8986da41ba0dcc6339156ee9a50175d2325bf
Anderson Costa <anderson.costa@mwangops.co.uk>
2024-06-30 12:13:00 +0100

    chore: extract repeated code to helper function

--- MailerTests/ContactsTableViewControllerTests.swift
+++ MailerTests/ContactsTableViewControllerTests.swift
@@ -14,52 +14,30 @@ final class ContactsTableViewControllerTests: XCTestCase {
 
     func test_fetch_contacts_on_load() {
         var didLoadContacts: Bool = false
-        let sut = ContactsTableViewController(
-            displayDetails: { _ in },
-            loadContacts: { _ in didLoadContacts = true }
-        )
-
-        sut.loadViewIfNeeded()
+        _ = makeSUT(loadContacts: { _ in didLoadContacts = true })
         
         XCTAssertTrue(didLoadContacts)
     }
 
     func test_table_is_empty_when_view_loads() {
-        let sut = ContactsTableViewController(
-            displayDetails: { _ in },
-            loadContacts: { _ in }
-        )
+        let sut = makeSUT()
 
-        sut.loadViewIfNeeded()
-        
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
     }
 
     func test_display_contacts_when_request_succeeds() {
-        let sut = ContactsTableViewController(
-            displayDetails: { _ in },
-            loadContacts: { completion in
-                let dummyContact = ContactViewModel(name: "DUMMY", contactMethod: "DUMMY")
-                let names = [dummyContact, dummyContact, dummyContact]
-                completion(names)
-            }
-        )
-
-        sut.loadViewIfNeeded()
+        let sut = makeSUT(loadContacts: { completion in
+            let dummyContact = ContactViewModel(name: "DUMMY", contactMethod: "DUMMY")
+            let names = [dummyContact, dummyContact, dummyContact]
+            completion(names)
+        })
         
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 3)
     }
 
     func test_display_correct_cell_information() throws {
         let contact = ContactViewModel(name: "James Cameron", contactMethod: "SMS")
-        let sut = ContactsTableViewController(
-            displayDetails: { _ in },
-            loadContacts: { completion in
-                completion([contact])
-            }
-        )
-
-        sut.loadViewIfNeeded()
+        let sut = makeSUT(loadContacts: { completion in completion([contact]) })
 
         let cell = try XCTUnwrap(sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0)))
         XCTAssertEqual(cell.textLabel?.text, contact.name)
@@ -69,19 +47,25 @@ final class ContactsTableViewControllerTests: XCTestCase {
     func test_present_detail_screen_when_cell_selected() {
         var selectedViewModel: ContactViewModel?
         let contact = ContactViewModel(name: "Percy Jackson", contactMethod: "Mail")
-        let sut = ContactsTableViewController(
-            displayDetails: { viewModel in
-                selectedViewModel = viewModel
-            },
-            loadContacts: { completion in
-                completion([contact])
-            }
+        let sut = makeSUT(
+            displayDetails: { viewModel in selectedViewModel = viewModel },
+            loadContacts: { completion in completion([contact]) }
         )
 
-        sut.loadViewIfNeeded()
         sut.tableView(sut.tableView, didSelectRowAt: IndexPath(row: 0, section: 0))
 
         XCTAssertEqual(selectedViewModel, contact)
+    }
+
+    // MARK: Helpers
+
+    private func makeSUT(
+        displayDetails: @escaping ContactsTableViewController.DisplayDetails = { _ in },
+        loadContacts: @escaping ContactsTableViewController.LoadContacts = { _ in }
+    ) -> ContactsTableViewController {
+        let viewController = ContactsTableViewController(displayDetails: displayDetails, loadContacts: loadContacts)
+        viewController.loadViewIfNeeded()
+        return viewController
     }
 }
 
