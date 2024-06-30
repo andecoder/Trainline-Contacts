3e7a498eb0ac81fd8304fd7d2b9b7534793d2573
Anderson Costa <anderson.costa@mwangops.co.uk>
2024-06-30 15:21:52 +0100

    feat: return email for Saint Lucia

--- MailerTests/Data/ContactMethodAdapterTests.swift
+++ MailerTests/Data/ContactMethodAdapterTests.swift
@@ -9,16 +9,28 @@
 import XCTest
 
 enum ContactMethod {
-    case sms
+    case email, sms
 }
 
 enum ContactMethodAdapter {
+    private static let separator = "|"
+
     static func method(from address: String) -> ContactMethod {
-        .sms
+        guard let country = address.components(separatedBy: separator).last else { return .sms }
+        if country == "Saint Lucia" {
+            return .email
+        }
+        return .sms
     }
 }
 
 final class ContactMethodAdapterTests: XCTestCase {
+
+    func test_method_is_email_for_saint_lucia() {
+        let fullAddress = "street|city|state|postcode|Saint Lucia"
+        let contactMethod = ContactMethodAdapter.method(from: fullAddress)
+        XCTAssertEqual(contactMethod, .email)
+    }
 
     func test_method_is_sms_for_other_countries() {
         let fullAddress = "street|city|state|postcode|Portugal"
