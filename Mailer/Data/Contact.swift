4621dd45c23775647215755414e186d55e07823d
Anderson Costa <anderson.costa@mwangops.co.uk>
2024-06-30 14:46:59 +0100

    chore: move types to separate files

---
+++ Mailer/Data/Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  Mailer
+//
+//  Created by Anderson Costa on 30/06/2024.
+//  Copyright © 2024 Trainline. All rights reserved.
+//
+
+import Foundation
+
+struct Contact: Equatable {
+    let fullName: String
+    let address: String
+    let phoneNumber: String
+    let email: String
+}
--- MailerTests/Data/ContactLoaderTests.swift
+++ MailerTests/Data/ContactLoaderTests.swift
@@ -10,41 +10,6 @@ import XCTest
 
 @testable import Mailer
 
-struct Contact: Equatable {
-    let fullName: String
-    let address: String
-    let phoneNumber: String
-    let email: String
-}
-
-struct ContactLoader {
-   
-    private let filePath: String
-    private let reader: CSVReading
-
-    init(filePath: String, reader: CSVReading) {
-        self.filePath = filePath
-        self.reader = reader
-    }
-
-    func load(completion: @escaping ([Contact]) -> Void) {
-        reader.open(path: filePath)
-        var contacts: [Contact] = []
-        while let row = reader.readNextRow() {
-            guard row.count == 4 else { continue }
-            let contact = Contact(
-                fullName: String(row[0]),
-                address: String(row[1]),
-                phoneNumber: String(row[2]),
-                email: String(row[3])
-            )
-            contacts.append(contact)
-        }
-        reader.close()
-        completion(contacts)
-    }
-}
-
 final class ContactLoaderTests: XCTestCase {
 
     func test_perform_actions_in_correct_order() {
