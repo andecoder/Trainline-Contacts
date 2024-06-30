e05c88f3944936043b981fd4ea5037e136f6eb98
Anderson Costa <anderson.costa@mwangops.co.uk>
2024-06-30 16:17:52 +0100

    feat: only load contacts once

--- MailerTests/Data/InMemoryContactLoaderCacheTests.swift
+++ MailerTests/Data/InMemoryContactLoaderCacheTests.swift
@@ -11,13 +11,16 @@ import XCTest
 final class InMemoryContactLoaderCache {
 
     private let loadContacts: () -> Void
+    private var loadCalled = false
 
     init(loadContacts: @escaping () -> Void) {
         self.loadContacts = loadContacts
     }
 
     func load() {
+        guard !loadCalled else { return }
         loadContacts()
+        loadCalled = true
     }
 }
 
@@ -28,5 +31,16 @@ final class InMemoryContactLoaderCacheTests: XCTestCase {
         let sut = InMemoryContactLoaderCache { loaderCalled = true }
         sut.load()
         XCTAssertTrue(loaderCalled)
+    }
+
+    func test_read_from_cache_on_subsequent_access() {
+        var loadCount = 0
+        let sut = InMemoryContactLoaderCache { loadCount += 1 }
+
+        sut.load()
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(loadCount, 1)
     }
 }
